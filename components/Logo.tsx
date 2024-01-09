8d4ed2da5542ab44fc754fd508773f306a953aa7
IftequarIffu <iftequar.154@gmail.com>
2024-01-09 13:13:26 +0530

    Modified the Logo component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Logo = () => {
-  return <div>Eventer</div>;
+  const title = "Eventer";
+  return (
+    <h1 className="text-gray-300 font-semibold">{title.toLocaleUpperCase()}</h1>
+  );
 };
 
 export default Logo;
