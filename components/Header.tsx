a3db167ab696d258d8167df715a92ad364f3386b
IftequarIffu <iftequar.154@gmail.com>
2024-01-11 09:54:48 +0530

    Added styling to Header.tsx. Lot to learn

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,34 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 
+const routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "All Events",
+    path: "/events/all",
+  },
+];
+
 const Header = () => {
   return (
-    <main>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <Link href="/">Home</Link>
-      <Link href="/events/all">All Events</Link>
-    </main>
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
