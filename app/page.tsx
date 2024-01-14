94e5ea0e379bf7523c911beed2c45238d00d15ab
IftequarIffu <iftequar.154@gmail.com>
2024-01-14 10:04:12 +0530

    Added the accent color in tailwind config and placed that in main.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </h1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
-        <span className="font-bold text-[#A4F839] italic underline">
+        <span className="font-bold text-accent italic underline">
           10,000 events
         </span>{" "}
         around you
@@ -17,7 +17,7 @@ export default function Home() {
 
       <form className="w-full lg:w-[580px]">
         <input
-          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#A4F839]/50 transition focus:ring-2 focus:bg-white/10"
+          className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
           placeholder="Search events in any city..."
           spellCheck={false}
         />
