f0b2086ad567695a9b3248c341162a1151d4ea21
IftequarIffu <iftequar.154@gmail.com>
2024-01-21 11:24:07 +0530

    Added the dynamic get city name

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import H1 from "@/components/H1";
 
-const EventsPage = () => {
+const EventsPage = ({ params: { city } }: { params: { city: string } }) => {
   return (
-    <main>
-      <H1>Events in Austin</H1>
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[100vh]">
+      <H1>Events in {city}</H1>
     </main>
   );
 };
