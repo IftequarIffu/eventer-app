2604216c2884b58bdcdc7de978f6ff535e58de30
IftequarIffu <iftequar.154@gmail.com>
2024-01-23 09:07:37 +0530

    Added All events functionality

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -4,7 +4,11 @@ import H1 from "@/components/H1";
 const EventsPage = ({ params: { city } }: { params: { city: string } }) => {
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[100vh]">
-      <H1>Events in {city}</H1>
+      <H1>
+        {city === "all"
+          ? "All Events"
+          : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
     </main>
   );
 };
