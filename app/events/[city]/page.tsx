a8236ddcbb001d40523ce9ca3bf3b8271285fdcb
IftequarIffu <iftequar.154@gmail.com>
2024-01-24 09:03:19 +0530

    Added the API call code

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import H1 from "@/components/H1";
+import { EventType } from "@/lib/types";
 
-const EventsPage = ({ params: { city } }: { params: { city: string } }) => {
+const EventsPage = async ({
+  params: { city },
+}: {
+  params: { city: string };
+}) => {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+
+  const events: EventType[] = await response.json();
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[100vh]">
       <H1>
@@ -9,6 +20,9 @@ const EventsPage = ({ params: { city } }: { params: { city: string } }) => {
           ? "All Events"
           : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 };
