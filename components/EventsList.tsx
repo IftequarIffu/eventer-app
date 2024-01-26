11f9d6ae99a254ae7aaa22cb819d6dc00e4da79f
IftequarIffu <iftequar.154@gmail.com>
2024-01-26 11:42:21 +0530

    Styled EventsList

--- components/EventsList.tsx
+++ components/EventsList.tsx
@@ -4,11 +4,11 @@ import EventCard from "./EventCard";
 
 const EventsList = ({ events }: { events: EventType[] }) => {
   return (
-    <>
+    <section className="flex max-w-[1100px] flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
-    </>
+    </section>
   );
 };
 
