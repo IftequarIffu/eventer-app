11d1481c97a09bae7ce06468da2852a590a865a8
IftequarIffu <iftequar.154@gmail.com>
2024-01-31 12:00:19 +0530

    Made changes to show only events related to a particular city

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -9,7 +9,7 @@ const EventsPage = async ({
   params: { city: string };
 }) => {
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
 
   const events: EventType[] = await response.json();
