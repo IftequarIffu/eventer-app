a3cb9a5d76a050a66ab95f8da681d112365bca14
IftequarIffu <iftequar.154@gmail.com>
2024-02-01 12:09:57 +0530

    Code for API call to get a particular event

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const EventPage = ({ params: { slug } }: { params: { slug: string } }) => {
-  return <main>Event name: {slug}</main>;
+const EventPage = async ({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) => {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+
+  const event = await response.json();
+
+  return <main>Event name: {event.description}</main>;
 };
 
 export default EventPage;
