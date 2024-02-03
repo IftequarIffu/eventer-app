4c7628e8050a346ab238bc4275d77a8e46fc03a0
IftequarIffu <iftequar.154@gmail.com>
2024-02-03 09:32:25 +0530

    Addes tyles to the event page

--- app/event/[slug]/page.tsx
+++ app/event/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import H1 from "@/components/H1";
 
 const EventPage = async ({
   params: { slug },
@@ -14,7 +15,7 @@ const EventPage = async ({
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
           src={event.imageUrl}
           className="object-cover z-0 blur-3xl "
@@ -24,13 +25,36 @@ const EventPage = async ({
           fill
           sizes="(max-width: 1280) 100vw, 1280px"
         />
-        <div className="z-1 relative">
+        <div className="z-1 flex flex-col lg:flex-row gap-x-6 lg:gap-16 relative">
           <Image
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
+            className="rounded-xl border-2 border-white/50 object-cover "
           />
+          <div className="flex flex-col ">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button
+              className="bg-white/10 text-lg capitalize lg:mt-auto 
+            mt-5 w-[95vw] sm:w-full py-2 rounded-md border-white/90 
+            border-2 bg-blur hover:scale-105 focus:scale-105 active:scale-[1.02] transition"
+            >
+              Get Tickets
+            </button>
+          </div>
         </div>
       </section>
       <div></div>
