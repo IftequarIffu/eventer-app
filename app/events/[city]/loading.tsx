fb4ba2a8f7e07014d1a7def9e46a9f61b6cec0e5
IftequarIffu <iftequar.154@gmail.com>
2024-02-09 10:40:05 +0530

    Modified the loading.tsx code of events/[city] to show multiple skeleton cards

--- app/events/[city]/loading.tsx
+++ app/events/[city]/loading.tsx
@@ -4,9 +4,9 @@ import SkeletonCard from "@/components/SkeletonCard";
 const Loading = () => {
   return (
     <div className="flex flex-wrap max-w-[1100px] justify-center mx-auto px-[20px] py-24 gap-20">
-      <SkeletonCard />
-      <SkeletonCard />
-      <SkeletonCard />
+      {Array.from({ length: 6 }).map((item, index) => (
+        <SkeletonCard key={index} />
+      ))}
     </div>
   );
 };
