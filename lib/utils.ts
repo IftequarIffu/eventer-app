be35a0631d830af138b3e9abe2a8a605cd27d762
IftequarIffu <iftequar.154@gmail.com>
2024-02-12 09:27:56 +0530

    Added a utility function for Capitalizing strings

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...classes: ClassValue[]) {
   return twMerge(clsx(classes));
 }
+
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
