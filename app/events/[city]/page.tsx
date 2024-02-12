ed753ad52cdebb64955d65fb965f7e8f744211e2
IftequarIffu <iftequar.154@gmail.com>
2024-02-12 10:18:24 +0530

    Added metadata for events/[city] endpoint

--- app/events/[city]/page.tsx
+++ app/events/[city]/page.tsx
@@ -1,8 +1,18 @@
 import React, { Suspense } from "react";
 import H1 from "@/components/H1";
-import { EventType } from "@/lib/types";
 import EventsList from "@/components/EventsList";
 import Loading from "./loading";
+import { capitalize } from "@/lib/utils";
+
+export function generateMetadata({
+  params: { city },
+}: {
+  params: { city: string };
+}) {
+  return {
+    title: `Events in ${capitalize(city)}`,
+  };
+}
 
 const EventsPage = async ({
   params: { city },
@@ -12,9 +22,7 @@ const EventsPage = async ({
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[100vh]">
       <H1 className="mb-28">
-        {city === "all"
-          ? "All Events"
-          : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city === "all" ? "All Events" : `Events in ${capitalize(city)}`}
       </H1>
       <Suspense fallback={<Loading />}>
         <EventsList city={city} />
