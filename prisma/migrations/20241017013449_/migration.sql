9c99dfb35a5b37af9d62275ea2cca654e8f69703
IftequarIffu <iftequar.154@gmail.com>
2024-10-17 07:07:38 +0530

    Changed the schema from sqlite to postgresql

---
+++ prisma/migrations/20241017013449_/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "organizerName" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Event_slug_key" ON "Event"("slug");
